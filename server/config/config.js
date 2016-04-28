bd92ce40e2089ba261e0872bcdbf0a5b950c5ee3
Andrew Throener <athroener@gmail.com>
2016-04-28 12:56:48 -0500

    feat(server): Master logging levels

--- server/config/config.js
+++ server/config/config.js
@@ -7,9 +7,9 @@ function load() {
     logs: {
       //https://github.com/winstonjs/winston
       winston: {
-        level:  process.env.MEAN_USERS_WINSTON_LEVEL || 'info', //{ error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
-        file: process.env.MEAN_USERS_WINSTON_FILE || './logs/users.log',
-        console: process.env.MEAN_USERS_WINSTON_CONSOLE || 'true'
+        level:  process.env.MEAN_USERS_WINSTON_LEVEL || process.env.MEAN_WINSTON_LEVEL || 'info', //{ error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
+        file: process.env.MEAN_USERS_WINSTON_FILE || process.env.MEAN_WINSTON_FILE || './logs/users.log',
+        console: process.env.MEAN_USERS_WINSTON_CONSOLE || process.env.MEAN_WINSTON_CONSOLE || 'true'
       }
     },
     mongoose: {
